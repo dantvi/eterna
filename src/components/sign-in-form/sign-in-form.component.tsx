356bbbce3bcefb7d42dad9d20e06d1c3dffae9fa
dantvi <daniel.tving@gmail.com>
2025-01-22 21:30:08 +0100

    create sign in form component with state management and form handling

---
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -0,0 +1,70 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import FormInput from '../form-input/Form-input.component';
+import Button from '../button/Button.component';
+import './sign-in-form.styles.scss';
+
+type FormFields = {
+  email: string;
+  password: string;
+};
+
+const defaultFormFields: FormFields = {
+  email: '',
+  password: '',
+};
+
+const SignInForm: React.FC = () => {
+  const [formFields, setFormFields] = useState<FormFields>(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      // Placeholder for API call
+      // Replace the with API integration
+      console.log('Attempting to sign in with:', { email, password });
+      resetFormFields();
+    } catch (error) {
+      console.error('Sign-in error', error);
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className='sign-in-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          onChange={handleChange}
+          name='email'
+          value={email}
+          required
+        />
+        <FormInput
+          label='Password'
+          type='password'
+          onChange={handleChange}
+          name='password'
+          value={password}
+          required
+        />
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
