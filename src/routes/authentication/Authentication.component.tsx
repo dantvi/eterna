df7e14d095b5031476a49c913ceeeed9143948b3
dantvi <daniel.tving@gmail.com>
2025-01-23 22:21:27 +0100

    implement styles for authentication component

--- src/routes/authentication/Authentication.component.tsx
+++ src/routes/authentication/Authentication.component.tsx
@@ -1,5 +1,6 @@
 import SignInForm from '../../components/sign-in-form/Sign-in-form.component';
 import SignUpForm from '../../components/sign-up-form/Sign-up-form.components';
+import './Authentication.styles.scss';
 
 const Authentication = () => {
   return (
