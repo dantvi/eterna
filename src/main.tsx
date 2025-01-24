c07f03a2657ef36ef7b837b570abd0cc58466747
dantvi <daniel.tving@gmail.com>
2025-01-24 16:14:43 +0100

    add products context and integrate it into main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ProductsProvider } from './contexts/products.context';
+import './index.scss';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
+  </StrictMode>
+);
