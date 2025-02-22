7ef3fe404e50eac0f2ebc2a7303a755e3cad266e
dantvi <daniel.tving@gmail.com>
2025-02-22 01:01:20 +0100

    added hamburger menu for mobile

---
+++ src/components/mobile-menu/Mobile-menu.component.tsx
@@ -0,0 +1,45 @@
+import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../contexts/auth.context';
+import { auth } from '../../utils/firebase.utils';
+import './Mobile-menu.styles.scss';
+
+const MobileMenu = () => {
+  const { currentUser } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const signOutHandler = async () => {
+    await auth.signOut();
+    setMenuOpen(false);
+  };
+
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <Menu
+      right
+      isOpen={menuOpen}
+      onStateChange={({ isOpen }) => setMenuOpen(isOpen)}
+    >
+      {currentUser ? (
+        <span className='mobile-user-email'>{currentUser.email}</span>
+      ) : null}
+      <Link className='menu-item' to={'/'} onClick={closeMenu}>HOME</Link>
+      <Link className='menu-item' to={'/shop'} onClick={closeMenu}>
+        SHOP
+      </Link>
+      {currentUser ? (
+        <Link className='menu-item' to={'/'} onClick={signOutHandler}>
+          SIGN OUT
+        </Link>
+      ) : (
+        <Link className='menu-item' to={'/auth'} onClick={closeMenu}>
+          SIGN IN
+        </Link>
+      )}
+    </Menu>
+  );
+};
+
+export default MobileMenu;
